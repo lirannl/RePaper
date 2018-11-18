b31d63129904cd7919d605d20fdcb439242d36ff
liran <liranpiade@gmail.com>
2018-11-18 18:13:48 +1000

    Initial commit - app takes text, prints it in a different activity, and shows an image of a cat if you type in Show the kitty.

---
+++ app/src/main/java/liran/com/wallpaper/DisplayMessageActivity.kt
@@ -0,0 +1,28 @@
+package liran.com.wallpaper
+
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+
+class DisplayMessageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_message)
+        val kittycode = "Show the kitty"
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            text = message
+        }
+      if (message == kittycode)
+       {
+        //Make the image visible
+        val imageView = findViewById<ImageView>(R.id.imageView).apply {
+            visibility = View.VISIBLE }
+       }
+
+    }
+}
